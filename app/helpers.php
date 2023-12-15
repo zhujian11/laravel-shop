2dab59c522518942046d47eedfb08332ea602c90
zhujian11 <595452769@qq.com>
2023-12-15 11:53:48 +0800

    基础布局

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
-function test_helper() {
-    return 'OK';
+// 将当前请求的路由名称转换为 CSS 类名称
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 首页
+Route::get('/', 'PagesController@root')->name('root');
