47350bb91a9e14d7cbcc683932a0ce7160512e62
zhujian11 <595452769@qq.com>
2023-12-15 15:23:29 +0800

    收货地址列表

---
+++ app/Models/UserAddress.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserAddress extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'province',
+        'city',
+        'district',
+        'address',
+        'zip',
+        'contact_name',
+        'contact_phone',
+        'last_used_at',
+    ];
+    protected $dates = ['last_used_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // 创建了一个访问器，在之后的代码里可以直接通过 $address->full_address 来获取完整的地址，而不用每次都去拼接。
+    public function getFullAddressAttribute()
+    {
+        return "{$this->province}{$this->city}{$this->district}{$this->address}";
+    }
+}
