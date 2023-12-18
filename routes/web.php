8b1422a4ed88b54530aa51e2d96b13dd993e98d7
zhujian11 <595452769@qq.com>
2023-12-18 17:24:05 +0800

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,10 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
     // 添加购物车
     Route::post('cart', 'CartController@add')->name('cart.add');
+    //查看购物车
+    Route::get('cart', 'CartController@index')->name('cart.index');
+    // 移除购物车中商品
+    Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 });
 
 // 商品列表
