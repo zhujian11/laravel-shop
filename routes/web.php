fed8c114551252324040a1927340184ebedf1159
zhujian11 <595452769@qq.com>
2023-12-21 11:44:05 +0800

    确认收货

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
     // 支付宝前端回调
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
+    // 确认收货
+    Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');
 });
 
 // 商品列表
