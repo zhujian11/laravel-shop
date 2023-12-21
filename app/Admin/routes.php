4d945a9614b718860b774e3a8f1dce497e0959cb
zhujian11 <595452769@qq.com>
2023-12-21 11:19:03 +0800

    管理后台 - 订单发货

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -18,5 +18,7 @@ Route::group([
     $router->post('products', 'ProductsController@store');
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->put('products/{id}', 'ProductsController@update');
-
+    $router->get('orders', 'OrdersController@index')->name('orders.index');
+    $router->get('orders/{order}', 'OrdersController@show')->name('orders.show');
+    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('orders.ship');
 });
