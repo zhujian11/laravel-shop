c18c083814a7e15cad8a1d4f10a9809fd236a639
zhujian11 <595452769@qq.com>
2023-12-22 00:22:22 +0800

    用户界面 - 检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('orders/{order}/review', 'OrdersController@sendReview')->name('orders.review.store');
     // 申请退款
     Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
+    // 检查优惠券
+    Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 });
 
 // 商品列表
