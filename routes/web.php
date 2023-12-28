01dd5ec958b7264171ed782b59bdc316cf2d9adc
zhujian11 <595452769@qq.com>
2023-12-28 14:48:22 +0800

    支付宝分期还款

--- routes/web.php
+++ routes/web.php
@@ -70,6 +70,10 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('installments', 'InstallmentsController@index')->name('installments.index');
     // 分期详情
     Route::get('installments/{installment}', 'InstallmentsController@show')->name('installments.show');
+    // 分期付款支付宝支付
+    Route::get('installments/{installment}/alipay', 'InstallmentsController@payByAlipay')->name('installments.alipay');
+    // 分期付款支付宝支付前端回调
+    Route::get('installments/alipay/return', 'InstallmentsController@alipayReturn')->name('installments.alipay.return');
 });
 
 // 商品列表
@@ -78,3 +82,5 @@ Route::get('products', 'ProductsController@index')->name('products.index');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show')->where(['product' => '[0-9]+']);
 // 支付宝服务器端回调
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
+// 分期付款支付宝支付后端回调
+Route::post('installments/alipay/notify', 'InstallmentsController@alipayNotify')->name('installments.alipay.notify');
