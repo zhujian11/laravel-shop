98bb267737a40b26997a05b946b2e1bcff40b564
zhujian11 <595452769@qq.com>
2023-12-28 11:29:31 +0800

    创建分期付款

--- routes/web.php
+++ routes/web.php
@@ -64,6 +64,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
     // 众筹商品下单
     Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
+    // 分期付款
+    Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
 });
 
 // 商品列表
