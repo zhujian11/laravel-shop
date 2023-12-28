252869486b827e07a3b3d7d26de9064f1abdbf15
zhujian11 <595452769@qq.com>
2023-12-28 15:59:33 +0800

    分期付款退款

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -12,6 +12,7 @@ use App\Jobs\CloseOrder;
 use Carbon\Carbon;
 use App\Models\CouponCode;
 use App\Exceptions\CouponCodeUnavailableException;
+use App\Jobs\RefundInstallmentOrder;
 
 class OrderService
 {
@@ -171,6 +172,14 @@ class OrderService
                         'refund_status' => Order::REFUND_STATUS_SUCCESS,
                     ]);
                 }
+                break;
+            case 'installment':
+                $order->update([
+                    'refund_no' => Order::getAvailableRefundNo(), // 生成退款订单号
+                    'refund_status' => Order::REFUND_STATUS_PROCESSING, // 将退款状态改为退款中
+                ]);
+                // 触发退款异步任务
+                dispatch(new RefundInstallmentOrder($order));
                 break;
             default:
                 // 原则上不可能出现，这个只是为了代码健壮性
